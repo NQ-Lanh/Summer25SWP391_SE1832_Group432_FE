6b1fbc120cd51387bbca305d7c8d113b590c9ef1
NQ-Lanh <quavipso1@gmail.com>
2025-06-01 18:12:48 +0700

    'login_register'

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import React from "react";
 import "antd/dist/reset.css";
-import LoginPage from "./LoginPage/Loginpage";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import RegisterPage from "./register/registerPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./login-page/LoginPage";
+import RegisterPage from "./register-page/RegisterPage";
 function App() {
   const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },{
-    path: "/login",
-    element: <LoginPage/>
-  },
-  {
-    path: "/register",
-    element: <RegisterPage/>
-  },
-
-]);
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
   return <RouterProvider router={router} />;
 }
 

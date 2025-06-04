f34f9df0fe778981b9783d8ffda9991b51a459b5
Aavengers4 <Longnvse180646@fpt.edu.vn>
2025-06-05 01:46:07 +0700

    'Long:homepage'

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,35 @@ import "antd/dist/reset.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./login-page/LoginPage";
 import RegisterPage from "./register-page/RegisterPage";
+import HomePage from "./pages/HomePage";
+import Blog1Detail from "./pages/Blog1Detail";
+import Blog2Detail from "./pages/Blog2Detail";
+import Blog3Detail from "./pages/Blog3Detail";
+import BlogDetail from "./pages/Detail1";
+import Detail2 from "./pages/Detail2";
+import Detail3 from "./pages/Detail3";
+import Header from "./components/Header";
+import ScrollToTop from "./ScrollToTop";
+
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: (
+        <>
+          <Header />
+          <HomePage />
+        </>
+      ),
+    },
+    {
+      path: "/home",
+      element: (
+        <>
+          <Header />
+          <HomePage />
+        </>
+      ),
     },
     {
       path: "/login",
@@ -17,8 +41,66 @@ function App() {
       path: "/register",
       element: <RegisterPage />,
     },
+    {
+      path: "/blog/hoat-dong-ngoai-khoa",
+      element: (
+        <>
+          <Header />
+          <Blog1Detail />
+        </>
+      ),
+    },
+    {
+      path: "/blog/ky-nang-song",
+      element: (
+        <>
+          <Header />
+          <Blog2Detail />
+        </>
+      ),
+    },
+    {
+      path: "/blog/cong-nghe-giao-duc",
+      element: (
+        <>
+          <Header />
+          <Blog3Detail />
+        </>
+      ),
+    },
+    {
+      path: "/blog/phong-ngua-benh",
+      element: (
+        <>
+          <Header />
+          <BlogDetail />
+        </>
+      ),
+    },
+    {
+      path: "/blog/dinh-duong-hoc-duong",
+      element: (
+        <>
+          <Header />
+          <Detail2 />
+        </>
+      ),
+    },
+    {
+      path: "/blog/suc-khoe-tam-than",
+      element: (
+        <>
+          <Header />
+          <Detail3 />
+        </>
+      ),
+    },
   ]);
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router}>
+      <ScrollToTop />
+    </RouterProvider>
+  );
 }
 
 export default App;
